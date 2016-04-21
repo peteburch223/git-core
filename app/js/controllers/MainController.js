2939e9ce2052d23e245556e23a8a4d308ed22623
Pete Burch <peteburch223@gmail>
2016-04-21 15:38:12 +0100

    tested for user data with static controller

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,3 +1,4 @@
 gitCoreApp.controller('MainController', [function() {
-  this.gitUsers = [{text: "Jazzy"}, {text: "Pete"}];
+  this.gitUsers = [{text: "Jazzy", repos: 35, followers: 45, avatarUrl: "https://avatars.githubusercontent.com/u/17009106?v=3" },
+                   {text: "Pete", repos: 27, followers: 19, avatarUrl: "https://avatars.githubusercontent.com/u/4344964?v=3"}];
 }]);
