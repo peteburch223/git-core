e42dbf689cf621a88e4f50af67bc5d4b6b21530e
Pete Burch <peteburch223@gmail>
2016-04-22 09:47:02 +0100

    all karma and protractor tests passing, need to refactor for async behaviour

--- test/e2e/gitcoreFeatures.js
+++ test/e2e/gitcoreFeatures.js
@@ -9,11 +9,11 @@ describe('Todos tracker', function() {
     var gitCoreUsers = element.all(by.repeater('gitUser in controller.gitUsers'));
     expect(gitCoreUsers.count()).toEqual(2);
     expect(gitCoreUsers.first().element(by.tagName('img')).getAttribute('src')).toEqual('https://avatars.githubusercontent.com/u/17009106?v=3');
-    expect(gitCoreUsers.first().element(by.css('.user-name')).getText()).toEqual('Jazzy');
+    expect(gitCoreUsers.first().element(by.css('.user-name')).getText()).toEqual('jazzysmith');
     expect(gitCoreUsers.first().element(by.css('.repos')).getText()).toEqual('35');
     expect(gitCoreUsers.first().element(by.css('.followers')).getText()).toEqual('45');
     expect(gitCoreUsers.last().element(by.tagName('img')).getAttribute('src')).toEqual('https://avatars.githubusercontent.com/u/4344964?v=3');
-    expect(gitCoreUsers.last().element(by.css('.user-name')).getText()).toEqual('Pete');
+    expect(gitCoreUsers.last().element(by.css('.user-name')).getText()).toEqual('petesmith');
     expect(gitCoreUsers.last().element(by.css('.repos')).getText()).toEqual('27');
     expect(gitCoreUsers.last().element(by.css('.followers')).getText()).toEqual('19');
 
