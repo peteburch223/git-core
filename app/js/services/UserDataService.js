6a0c944d0eb5559275c7f16b261ac85b33cae3d5
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-22 11:54:06 +0100

    Service for retreiving user data from one username

--- app/js/services/UserDataService.js
+++ app/js/services/UserDataService.js
@@ -1,20 +1,17 @@
+
 gitCoreApp.service('UserDataService', ['$http', function($http){
   var self = this;
 
-  self.getData = function(usernames){
-    return usernames.map(function(username){
-      return $http.get('https://api.github.com/users/'+username)
-      .then(_handleResponseFromApi);
-    });
+  self.getData = function(username){
+    return $http.get('https://api.github.com/users/' + username + '?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3')
+    .then(_handleResponseFromApi);
+
+    function _handleResponseFromApi(response){
+        var userData = response.data;
+        return {username: userData.login,
+                avatar_url: userData.avatar_url,
+                followers: userData.followers,
+                public_repos: userData.public_repos};
+    }
   };
-
-  function _handleResponseFromApi(response){
-    return response.data.map(function(){
-      return {username: userData.login,
-              avatar_url: userData.avatar_url,
-              followers: userData.followers,
-              public_repos: userData.public_repos};
-    });
-  }
-
 }]);
--- test/unit/UserDataService.spec.js
+++ test/unit/UserDataService.spec.js
@@ -4,35 +4,9 @@ describe('UserDataService', function() {
   var UserDataService, httpBackend;
 
   var userData = { "login": "JojSh",
-                  "id": 16098730,
                   "avatar_url": "https://avatars.githubusercontent.com/u/16098730?v=3",
-                  "gravatar_id": "",
-                  "url": "https://api.github.com/users/JojSh",
-                  "html_url": "https://github.com/JojSh",
-                  "followers_url": "https://api.github.com/users/JojSh/followers",
-                  "following_url": "https://api.github.com/users/JojSh/following{/other_user}",
-                  "gists_url": "https://api.github.com/users/JojSh/gists{/gist_id}",
-                  "starred_url": "https://api.github.com/users/JojSh/starred{/owner}{/repo}",
-                  "subscriptions_url": "https://api.github.com/users/JojSh/subscriptions",
-                  "organizations_url": "https://api.github.com/users/JojSh/orgs",
-                  "repos_url": "https://api.github.com/users/JojSh/repos",
-                  "events_url": "https://api.github.com/users/JojSh/events{/privacy}",
-                  "received_events_url": "https://api.github.com/users/JojSh/received_events",
-                  "type": "User",
-                  "site_admin": false,
-                  "name": "Joseph Sharratt",
-                  "company": null,
-                  "blog": null,
-                  "location": "London",
-                  "email": null,
-                  "hireable": null,
-                  "bio": null,
                   "public_repos": 29,
-                  "public_gists": 0,
                   "followers": 9,
-                  "following": 14,
-                  "created_at": "2015-12-01T10:26:49Z",
-                  "updated_at": "2016-04-07T07:17:28Z"
                   };
 
   beforeEach(inject(function(_UserDataService_, $httpBackend) {
@@ -41,13 +15,12 @@ describe('UserDataService', function() {
   }));
 
   it('returns user data', function(){
-    httpBackend.expectGET('https://api.github.com/users/jojsh').respond(userData);
-    console.log(UserDataService.getData(['JojSh']));
+    httpBackend.expectGET('https://api.github.com/users/JojSh?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3').respond(userData);
     UserDataService.getData(['JojSh']).then(function(results){
-      expect(results).toEqual([{ username:'JojSh',
-                                 avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
-                                 followers: 9,
-                                 public_repos: 29 }]);
+      expect(results).toEqual({ username:'JojSh',
+                                avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
+                                followers: 9,
+                                public_repos: 29 });
     });
     httpBackend.flush();
   });
