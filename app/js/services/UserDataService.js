af2db3d7e6514e1446516883d52fe94800d1d008
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-22 14:37:02 +0100

    it works yay :|

--- app/js/services/UserDataService.js
+++ app/js/services/UserDataService.js
@@ -1,17 +1,41 @@
+// gitCoreApp.service('UserDataService', ['$http', function($http){
+//   var self = this;
+//
+//   self.getData = function(username){
+//     return $http.get('https://api.github.com/users/' + username + '?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3')
+//     .then(_handleResponseFromApi);
+//
+//     function _handleResponseFromApi(response){
+//         var userData = response.data;
+//         return {username: userData.login,
+//                 avatar_url: userData.avatar_url,
+//                 followers: userData.followers,
+//                 public_repos: userData.public_repos};
+//     }
+//   };
+// }]);
 
 gitCoreApp.service('UserDataService', ['$http', function($http){
   var self = this;
 
-  self.getData = function(username){
-    return $http.get('https://api.github.com/users/' + username + '?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3')
-    .then(_handleResponseFromApi);
-
-    function _handleResponseFromApi(response){
-        var userData = response.data;
-        return {username: userData.login,
-                avatar_url: userData.avatar_url,
-                followers: userData.followers,
-                public_repos: userData.public_repos};
-    }
+  self.getData = function(usernames){
+    return usernames.map(_getUserData);
   };
+
+  function _getUserData(username){
+    return _getApiData(username)
+      .then(_handleResponseFromApi);
+  }
+
+  function _getApiData(username){
+    return $http.get('https://api.github.com/users/' + username + '?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3');
+  }
+
+  function _handleResponseFromApi(response){
+    var userData = response.data;
+    return {username: userData.login,
+            avatar_url: userData.avatar_url,
+            followers: userData.followers,
+            public_repos: userData.public_repos};
+  }
 }]);
--- test/unit/UserDataService.spec.js
+++ test/unit/UserDataService.spec.js
@@ -3,24 +3,62 @@ describe('UserDataService', function() {
 
   var UserDataService, httpBackend;
 
-  var userData = { "login": "JojSh",
-                  "avatar_url": "https://avatars.githubusercontent.com/u/16098730?v=3",
-                  "public_repos": 29,
-                  "followers": 9,
-                  };
+  var JojShUserData = { "login": "JojSh",
+                         "avatar_url": "https://avatars.githubusercontent.com/u/16098730?v=3",
+                         "public_repos": 29,
+                         "followers": 9,
+                       };
+
+ var rhiannonruthUserData = { "login": "rhiannonruth",
+                              "avatar_url": "rhiannonURL",
+                              "public_repos": 50,
+                              "followers": 1000,
+                            };
 
   beforeEach(inject(function(_UserDataService_, $httpBackend) {
     UserDataService = _UserDataService_;
     httpBackend = $httpBackend;
   }));
 
-  it('returns user data', function(){
-    httpBackend.expectGET('https://api.github.com/users/JojSh?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3').respond(userData);
-    UserDataService.getData(['JojSh']).then(function(results){
+  it('returns user data for one user', function(){
+    httpBackend.expectGET('https://api.github.com/users/JojSh?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3').respond(JojShUserData);
+    // UserDataService.getData(['JojSh']).then(function(results){
+    //   expect(results).toEqual({ username:'JojSh',
+    //                             avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
+    //                             followers: 9,
+    //                             public_repos: 29 });
+    // });
+    UserDataService.getData(['JojSh'], function(results){
       expect(results).toEqual({ username:'JojSh',
                                 avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
                                 followers: 9,
                                 public_repos: 29 });
+                              });
+    httpBackend.flush();
+  });
+
+  it('returns user data for several users', function(){
+    httpBackend.expectGET('https://api.github.com/users/JojSh?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3').respond(JojShUserData);
+    httpBackend.expectGET('https://api.github.com/users/rhiannonruth?access_token=eed410d933e9c4e65a6a02d6db1a12bdf66545b3').respond(rhiannonruthUserData);
+    // UserDataService.getData(['JojSh', 'rhiannonruth']).then(function(results){
+    //   expect(results).toEqual([{ username:'JojSh',
+    //                              avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
+    //                              followers: 9,
+    //                              public_repos: 29 },
+    //                            { username:'rhiannonruth',
+    //                              avatar_url: "rhiannonURL",
+    //                              followers: 1000,
+    //                              public_repos: 50 }]);
+    // });
+    UserDataService.getData(['JojSh', 'rhiannonruth'], function(results) {
+      expect(results).toEqual([{ username:'JojSh',
+                                 avatar_url: "https://avatars.githubusercontent.com/u/16098730?v=3",
+                                 followers: 9,
+                                 public_repos: 29 },
+                               { username:'rhiannonruth',
+                                 avatar_url: "rhiannonURL",
+                                 followers: 1000,
+                                 public_repos: 50 }]);
     });
     httpBackend.flush();
   });
